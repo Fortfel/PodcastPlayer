be3a92d44de2b70fc63d0602e0e389899da635fc
Marcin Dubiel <fortfel@gmail.com>
2025-12-01 18:30:53 +0100

    update: updated home index

--- apps/web/src/routes/_app/index.tsx
+++ apps/web/src/routes/_app/index.tsx
@@ -1,9 +1,11 @@
-import * as React from 'react'
+import type * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 
-import { useResizeObserver } from '@workspace/ui/hooks/use-resize-observer'
 import { cn } from '@workspace/ui/lib/utils'
+
+import { Player } from '@/routes/_app/-components/layout/player'
 import { PodcastsList } from '@/routes/_app/-components/layout/podcasts-list'
+import { Queque } from '@/routes/_app/-components/layout/queque'
 import { Search } from '@/routes/_app/-components/layout/search'
 
 export const Route = createFileRoute('/_app/')({
@@ -12,20 +14,19 @@ export const Route = createFileRoute('/_app/')({
 
 function HomeComponent(): React.JSX.Element {
   const shadowClass = 'shadow-[0_15px_30px_5px_rgba(0,0,0,0.3)]'
-  const [searchRect, setSearchRect] = React.useState<DOMRectReadOnly | null>(null)
-  const searchRef = React.useRef<HTMLDivElement>(null)
 
-  useResizeObserver(searchRef, (entry) => {
-    setSearchRect(entry.contentRect)
-  })
 
   return (
-    <div className="grid flex-1 grid-cols-1 grid-rows-[auto_400px_1fr] gap-5 md:grid-cols-[minmax(0,1fr)_var(--container-2xs)] lg:grid-cols-[minmax(0,1fr)_var(--container-sm)]">
-      <Search ref={searchRef} className="mb-5" />
+    <div className="grid max-h-[calc(100dvh-var(--nav-height)-50px)] flex-1 grid-cols-1 grid-rows-[auto_400px_1fr] gap-5 gap-x-8 md:grid-cols-[minmax(0,1fr)_var(--container-2xs)] lg:grid-cols-[minmax(0,1fr)_var(--container-sm)]">
+      <Search className="mb-5" />
 
-      <PodcastsList className={cn(shadowClass, 'row-span-2 row-start-2')} searchRect={searchRect} />
-      <Player className={cn(shadowClass, 'hidden md:col-start-2 md:row-span-1 md:row-start-2 md:flex')} />
-      <div className={cn(shadowClass, 'hidden md:col-start-2 md:row-span-1 md:row-start-3 md:flex')}>last one</div>
+      <PodcastsList className={cn(shadowClass, 'row-span-2 row-start-2 overflow-y-auto')} />
+      <Player className={cn(shadowClass, 'hidden md:col-start-2 md:row-span-1 md:row-start-2 md:block')} />
+      <Queque
+        className={cn(shadowClass, 'hidden overflow-y-auto md:col-start-2 md:row-span-1 md:row-start-3 md:block')}
+      >
+        last one
+      </Queque>
     </div>
   )
 }
