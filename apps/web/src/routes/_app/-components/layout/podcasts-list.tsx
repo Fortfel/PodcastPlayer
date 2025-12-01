1ea382cfb86201ec28687a4dfe1d3adcaca3c5dc
Marcin Dubiel <fortfel@gmail.com>
2025-12-01 18:28:31 +0100

    update: updated podcastslist component

--- apps/web/src/routes/_app/-components/layout/podcasts-list.tsx
+++ apps/web/src/routes/_app/-components/layout/podcasts-list.tsx
@@ -3,29 +3,20 @@ import type * as React from 'react'
 import { ScrollArea } from '@workspace/ui/components/scroll-area'
 import { cn } from '@workspace/ui/lib/utils'
 
-const PodcastsList = ({
-  searchRect,
-  className,
-  ...props
-}: React.ComponentProps<'div'> & { searchRect: DOMRectReadOnly | null }) => {
-  const searchHeight = `${(searchRect?.height ?? 0).toString()}px`
-
+const PodcastsList = ({ className, ...props }: React.ComponentProps<'div'>) => {
   return (
-    <div data-slot="podcast-card" className={cn('bg-muted @container rounded-lg', className)} {...props}>
-      <ScrollArea
-        className="h-[calc(100dvh-var(--nav-height)-var(--search-height)-90px)]"
-        style={{ '--search-height': searchHeight } as React.CSSProperties}
-      >
+    <div data-slot="podcasts-list" className={cn('bg-muted @container rounded-lg', className)} {...props}>
+      <ScrollArea className="h-full">
         <div className="flex flex-col gap-6 p-4 sm:p-6">
           pulsing podcast icon here
-          <div className="bg-card @max-md:flex-col @max-md:items-center flex items-start gap-2 rounded-lg shadow-xl">
+          <div className="bg-card @max-md:flex-col @max-md:items-center flex items-start gap-2 rounded-lg border shadow-xl">
             <img
               src="/default-podcast.png"
               alt="default-podcast"
-              className="shrink-1 size-full min-w-32 max-w-60 object-cover"
+              className="shrink-1 @max-md:max-w-52 aspect-square size-full min-w-32 max-w-60 object-cover"
             />
-            <ScrollArea className="@max-md:flex-col @max-md:min-w-0 flex h-60 min-w-64 flex-1 p-4">
-              <div className="text-muted-foreground flex flex-col gap-2">
+            <ScrollArea className="@max-md:flex-col @max-md:min-w-0 flex min-w-64 flex-1 p-4">
+              <div className="text-muted-foreground flex h-52 flex-col gap-2">
                 <h2 className="@max-md:text-center text-foreground text-xl font-semibold">Title</h2>
                 <p className="text-sm/4.5 text-justify">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus delectus dignissimos doloremque
