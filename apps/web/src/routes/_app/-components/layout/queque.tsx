d9a5a7b1c7d85aed440ecf9fef375c942b19dd87
Marcin Dubiel <fortfel@gmail.com>
2025-12-04 13:39:50 +0100

    update: updated queque

--- apps/web/src/routes/_app/-components/layout/queque.tsx
+++ apps/web/src/routes/_app/-components/layout/queque.tsx
@@ -10,14 +10,26 @@ const Queque = ({ className, ...props }: React.ComponentProps<'div'>) => {
     <div data-slot="queque" className={cn('bg-muted rounded-lg', className)} {...props}>
       <ScrollArea className="h-full">
         <ItemGroup className="gap-4 p-4">
-          <Item variant="outline" className="bg-card rounded-lg p-0 shadow-xl" role="listitem">
+          <Item variant="outline" className="bg-card gap-4 rounded-lg p-0 shadow-xl" role="listitem">
             <ItemMedia>
               <img src="/default-podcast.png" alt="default-podcast" className="max-w-25 aspect-square object-cover" />
             </ItemMedia>
             <ItemContent>
-              <ItemTitle>Podcast Title Podcast Title Podcast Title Podcast Title Podcast Title Podcast</ItemTitle>
+              <ItemTitle className="text-sm">Podcast Title Podcast Title Podcast Title</ItemTitle>
             </ItemContent>
-            <ItemContent className="gap-2 px-4">
+            <ItemContent className="gap-2 px-3">
+              <CirclePlayIcon size={30} className="text-success" />
+              <Trash2Icon size={30} className="text-destructive" />
+            </ItemContent>
+          </Item>
+          <Item variant="outline" className="bg-card gap-4 rounded-lg p-0 shadow-xl" role="listitem">
+            <ItemMedia>
+              <img src="/default-podcast.png" alt="default-podcast" className="max-w-25 aspect-square object-cover" />
+            </ItemMedia>
+            <ItemContent>
+              <ItemTitle className="text-sm">Podcast Title Podcast Title Podcast Title</ItemTitle>
+            </ItemContent>
+            <ItemContent className="gap-2 px-3">
               <CirclePlayIcon />
               <Trash2Icon />
             </ItemContent>
