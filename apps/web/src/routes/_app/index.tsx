bb7d659bc0100fe6b494c40f94b19f94d8b9cc30
Marcin Dubiel <fortfel@gmail.com>
2025-12-07 20:06:16 +0100

    update: updated home page

--- apps/web/src/routes/_app/index.tsx
+++ apps/web/src/routes/_app/index.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react'
+import type * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 
-import type { RouterOutputs } from '@workspace/api/server'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@workspace/ui/components/tabs'
 import { useMediaQuery } from '@workspace/ui/hooks/use-media-query'
 import { cn } from '@workspace/ui/lib/utils'
@@ -17,22 +16,14 @@ export const Route = createFileRoute('/_app/')({
 
 function HomeComponent(): React.JSX.Element {
   const isDesktop = useMediaQuery('(min-width: 1024px)')
-  const [searchResult, setSearchResult] = React.useState<RouterOutputs['podcastIndex']['searchPodcastByTerm'] | null>(
-    null,
-  )
-  const [isLoading, setIsLoading] = React.useState(false)
 
   const shadowClass = 'shadow-[0_15px_30px_5px_rgba(0,0,0,0.3)]'
 
-  const handleSearchResult = (result: RouterOutputs['podcastIndex']['searchPodcastByTerm']) => {
-    setSearchResult(result)
-  }
-
   return isDesktop ? (
     <div className="grid max-h-[max(800px,calc(100dvh-var(--nav-height)-50px))] grid-cols-[minmax(0,1fr)_var(--container-sm)] grid-rows-[auto_400px_1fr] gap-5 gap-x-8">
-      <Search className="-mr-24 mb-5" onSearch={handleSearchResult} onLoadingChange={setIsLoading} />
+      <Search className="-mr-24 mb-5" />
 
-      <PodcastsList podcasts={searchResult} isLoading={isLoading} className={cn(shadowClass, 'row-span-2 row-start-2')} />
+      <PodcastsList className={cn(shadowClass, 'row-span-2 row-start-2')} />
       <Player className={cn(shadowClass, 'col-start-2 row-span-1 row-start-2')} />
       <Queque className={cn(shadowClass, 'col-start-2 row-span-1 row-start-3 overflow-y-auto')} />
     </div>
@@ -43,8 +34,8 @@ function HomeComponent(): React.JSX.Element {
         <TabsTrigger value="listen">Listen</TabsTrigger>
       </TabsList>
       <TabsContent value="search" className="row-start-2 grid grid-rows-[auto_1fr]">
-        <Search className="mb-5" onSearch={handleSearchResult} onLoadingChange={setIsLoading} />
-        <PodcastsList podcasts={searchResult} isLoading={isLoading} className={cn(shadowClass, '')} />
+        <Search className="mb-5" />
+        <PodcastsList className={cn(shadowClass, '')} />
       </TabsContent>
       <TabsContent value="listen" className="row-start-2 grid grid-rows-[auto_1fr] gap-8">
         <Player
